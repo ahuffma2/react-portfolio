efbfa81271913596c955440705076a0ab286c297
Austin Huffman <austinhuffman0@gmail.com>
2021-08-20 19:42:44 -0400

    Tailwind Imported and working images for projects imported to data.js Descriptions added Skills rounded out

--- src/data.js
+++ src/data.js
@@ -4,24 +4,38 @@ export const projects = [
       title: "Haunted Doll House",
       subtitle: "Frontend & Backend using Sequelize",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project-1.gif",
-      link: "https://reactbootcamp.com",
+        "Haunted Dollhouse is the leading online retailer of doll vessels with attached spirits so active they're guaranteed to leave a pit of inconceivable dread in your stomach be a friend for life!",
+      image: "/images/Haunted-Ico.jpg",
+      link: "https://haunted-doll-house.herokuapp.com/",
     },
     {
       title: "Comics Assembled",
       subtitle: "Team Lead using HTML, CSS, and API integration",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project-2.gif",
-      link: "https://reedbarger.com",
+        "I guided this team on making a website from scratch using HTML, CSS, Javascript, Bootstrap, along with Marvel API and Google Books API. While most of the code wasn't directly mine, I had a hand in almost all of it. I created all documentation, kanban boards, Managed the Git repo, resident question answerer. I learned a lot about what it takes to manage a team. You need concrete coding standard. You need documentation and ensure that each of your members is adhering to documentation. All preformed in a single Agile Sprint in a week.",
+      image: "/images/Comics-1.jpeg",
+      link: "www.comics-assembled.com",
     },
     {
       title: "Vesta Absolution",
       subtitle: "Team Lead Senior Project",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project-3.gif",
-      link: "https://jsbootcamp.com",
+        "This project was a huge endeavor with me being the lead programmer and having a hand in every single system we created. I learned about Git and version control which, hoo-boy, with students is a task and a half. The meat and potatoes for this game however was my, custom OpenGL shader programming, Animation Rigging and Animation State Management programming, Character Controllers, Level Design, 3D Character Modelling, and AI. Many of these systems I learned and improved on as we progressed. We also did weekly sprints following an Agile Methodology, adhering to the core principles.",
+      image: "/images/Vesta-Ico.jpg",
+      link: "https://Austinhuffman0.itch.io",
     },
 ];
+
+export const skills = [
+
+    'Javascript',
+    'React',
+    'MongoDB',
+    'Express',
+    'HTML',
+    'CSS',
+    'Jquery',
+    'NextJS',
+    'C#',
+    'SQL',
+]
